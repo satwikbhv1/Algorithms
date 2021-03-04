f8340db69794c11231e388ac9bbec468601994e3
satwikbhv1 <bhamidi.venkat@gmail.com>
2021-03-04 09:56:28 +0530

    Undirected graph creation using adjaceny list in cpp

---
+++ createUndirectedGraph.cpp
@@ -0,0 +1,21 @@
+//author:satwik_bhv1
+//creating undirected_graph using adjacencylist
+/*
+	edge a->b is same as edge b->a
+*/
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> ar[10001];//adjaceny list(array of vectors)=>data structure used for creating graph
+int main()
+{
+	int n;//number of nodes
+	int m;//number of edges
+	cin>>n>>m;
+	for(int i=0;i<m;i++)
+	{
+		int a,b;
+		cin>>a>>b;
+		ar[a].push_back(b);
+		ar[b].push_back(a);
+	}
+}
