bddf50919adfc42a1f2d61aff9093911bd58cd40
satwikbhv1 <32234222+satwikbhv1@users.noreply.github.com>
2021-12-03 23:39:24 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,29 +1,27 @@
 //This is DSU class is made using both path compression and union by rank.  
 class DSU
 {
-    vector<int> par,_rank;
-    DSU(int n)
-    {
-        par.resize(n+1,-1);
-        _rank.resize(n+1,1);
-    }
-    int find(int x)
-    {
-        if(par[x]==-1) return x;
-        return par[x]=find(par[x]);
-    }
-    void merge(int x,int y)
-    {
-        int a=find(x);
-        int b=find(y);
-        if(a!=b)
+    private:
+        vector<int> par,_rank;
+    public:
+        DSU(int n)
         {
-            if(_rank[a]>_rank[b])
-            {
-                swap(a,b);
-            }
-            par[b]=a;
-            _rank[a]+=_rank[b];
+            par.resize(n+1,-1);
+            _rank.resize(n+1,1);
         }
-    }
+        int find(int x)
+        {
+            if(par[x]==-1) return x;
+            return par[x]=find(par[x]);
+        }
+        void merge(int x,int y)
+        {
+            int a=find(x),b=find(y);
+            if(a!=b)
+            {
+                if(_rank[a]>_rank[b]) swap(a,b);
+                par[b]=a;
+                _rank[a]+=_rank[b];
+            }
+        }
 }
